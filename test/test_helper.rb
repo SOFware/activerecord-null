b618509343198329516f4ac75f70e93d1fddd6e7
Jim Gay <jim@saturnflyer.com>
2024-10-25 14:53:15 -0400

    Require null after simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,10 @@
 require "simplecov" if ENV["CI"]
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "activerecord/null"
 
 require "active_record"
 ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
 load "test/support/schema.rb"
-
-require "activerecord/null"
 
 require "minitest/autorun"
